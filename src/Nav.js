3bd2e4eb594944a3933d50498a6dd87570238597
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2022-12-15 23:10:28 +0200

    first

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import './App.css';
+function Nav() {
+    return (
+        <div class="container red topBotomBordersOut">
+            <a>HOME</a>
+            <a>ARTICLES</a>
+            <a>PORTFOLIO</a>
+            <a>ABOUT</a>
+            <a>CONTACT</a>
+        </div>
+    )
+}
+export default Nav;
